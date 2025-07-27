ebc4b13db0688c6f958e4dc74bd2a3892e91e917
Utpal <utpalsen902@gmail.com>
2025-07-27 17:50:23 +0530

    charts added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
+import  AppAreaChart  from "@/components/AppAreaChart";
+import { AppBarChart } from "@/components/AppBarChart";
+import AppPieChart from "@/components/AppPieChart";
+
 const page = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">
         test
       </div>
-      <div className="bg-primary-foreground p-4 rounded-lg ">test</div>
-      <div className="bg-primary-foreground p-4 rounded-lg ">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg "><AppPieChart/></div>
       <div className="bg-primary-foreground p-4 rounded-lg ">test</div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
-        test
+        <AppAreaChart />
       </div>
       <div className="bg-primary-foreground p-4 rounded-lg ">test</div>
     </div>
