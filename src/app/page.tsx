1edaec5fe808bc9dbae081dc757463ed0f077686
Utpal <utpalsen902@gmail.com>
2025-07-27 17:28:02 +0530

    grid layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
+const page = () => {
   return (
-    <div className="h-screen flex items-center justify-center">
-     <Button variant='destructive' >Click me</Button>
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        test
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        test
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">test</div>
     </div>
   );
-}
+};
+
+export default page;
